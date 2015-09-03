628f23664bed2756749f427092fce069d50a14a0
Tyler Shambora <tyler@bvaccel.com>
2015-09-02 21:34:02 -0700

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -28,7 +28,7 @@ var plumberErrorHandler = {
 };
 
 gulp.task('styles', function() {
-  return gulp.src(['dev/styles/main.scss'])
+  return gulp.src(['dev/styles/*.scss'])
     .pipe(plumber(plumberErrorHandler))
     .pipe(sourcemaps.init())
     .pipe(sass({ errLogToConsole: true }))
@@ -40,7 +40,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-  return gulp.src(['dev/scripts/**'])
+  return gulp.src(['dev/scripts/*.js'])
     .pipe(plumber(plumberErrorHandler))
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
@@ -56,29 +56,39 @@ gulp.task('vendor', function() {
   return gulp.src(['dev/vendor/**'])
     .pipe(plumber(plumberErrorHandler))
     .pipe(styles)
+    .pipe(sourcemaps.init())
     .pipe(sass({ errLogToConsole: true }))
     .pipe(cssmin())
+    .pipe(sourcemaps.write())
     .pipe(styles.restore())
     .pipe(scripts)
+    .pipe(sourcemaps.init())
     .pipe(concat('vendor.js'))
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(scripts.restore())
     .pipe(flatten())
-    .pipe(gulp.dest('deploy/assets'));
-});
-
-gulp.task('imagemin', function() {
-  return gulp.src(['dev/images/*'])
-    .pipe(changed('deploy/assets'))
-    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
     .pipe(gulp.dest('deploy/assets'));
 });
 
 gulp.task('copy', function() {
   return gulp.src(['dev/liquid/**'], {base: 'dev/liquid'})
     .pipe(plumber(plumberErrorHandler))
-    .pipe(changed('deploy/'))
+    .pipe(changed('deploy/', {hasChanged: changed.compareSha1Digest}))
     .pipe(gulp.dest('deploy/'));
+});
+
+gulp.task('imagecopy', function() {
+  return gulp.src(['dev/images/*'])
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(changed('deploy/assets', {hasChanged: changed.compareSha1Digest}))
+    .pipe(gulp.dest('deploy/assets'));
+});
+
+gulp.task('imagemin', function() {
+  return gulp.src(['dev/images/*'])
+    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dev/images/'));
 });
 
 gulp.task('clean', function(cb) {
@@ -86,28 +96,25 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('build', ['clean'], function(cb) {
-  runsequence(['copy', 'styles', 'scripts', 'vendor', 'imagemin'], cb);
+  runsequence(['copy', 'imagecopy', 'styles', 'scripts', 'vendor'], cb);
 });
 
 gulp.task('watch', ['build'], function() {
   gulp.watch(['dev/styles/**/*.scss'], ['styles']);
   gulp.watch(['dev/scripts/**/*.js'], ['scripts']);
   gulp.watch(['dev/vendor/**/*.{js,scss}'], ['vendor']);
-  gulp.watch(['dev/images/**'], ['imagemin']);
+  gulp.watch(['dev/images/*'], ['imagecopy']);
   gulp.watch(['dev/liquid/**'], ['copy']);
 });
 
 gulp.task('upload', ['watch'], function() {
-  if (!argv.env) {
-    return false;
-  } else if (argv.env && config.shopify.hasOwnProperty(argv.env)) {
+  if (argv.env && config.shopify.hasOwnProperty(argv.env)) {
     env = config.shopify[argv.env];
   } else {
-    env = config.shopify.staging;
+    return false;
   }
-  return watch('deploy/+(assets|layout|config|snippets|templates|locales)/**')
+  return watch('deploy/{assets|layout|config|snippets|templates|locales}/**')
     .pipe(shopify(env.apiKey, env.password, env.url, env.themeId, env.options));
 });
 
 gulp.task('default', ['clean', 'build', 'watch', 'upload']);
-
